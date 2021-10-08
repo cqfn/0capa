b4ba18039cf36a18e7a376f734082100894800be
Xavier Vasquez <xavzelada@gmail.com>
2021-10-08 13:32:35 +0300

    settings reading

--- chatbot/app/controllers/api/v1/issues_controller.rb
+++ chatbot/app/controllers/api/v1/issues_controller.rb
@@ -7,12 +7,12 @@ class Api::V1::IssuesController < ApplicationController
 
   def create
     activations = TomRadarActivation.find_by(status: "Pending")
-
+    settings = TomSetting.find_by(agentname: 'github')
 
 
     TomRadarActivation.where(status: "Pending").each do | issue |
       puts issue.id
-      response = HTTP[:accept => "application/vnd.github.v3+json",:Authorization => "token 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" ].post('https://api.github.com/repos/xavzelada/repo_test/issues', :json => { :title => issue.issuetitle, :body => issue.issuebody })
+      response = HTTP[:accept => "application/vnd.github.v3+json",:Authorization => "token " + settings.apisecret ].post('https://api.github.com/repos/xavzelada/repo_test/issues', :json => { :title => issue.issuetitle, :body => issue.issuebody })
       puts JSON.pretty_generate( response)
       issue.status = "Success"
       issue.save
