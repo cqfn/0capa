6639a6a3bca15ad982b6b0dcf949a83803b748c6
Xavier Vasquez <xavzelada@gmail.com>
2021-10-08 11:16:04 +0300

    routes

--- radar/config/routes.rb
+++ radar/config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'webhook/index'
-      get 'webhook/show'
+      resources :webhook, only: [:index, :show, :create]
     end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
