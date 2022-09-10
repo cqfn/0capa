cce17d4ab49d1cb99a89d9454e9b3a4aff1aa83f
doldrums <doldrums.317@gmail.com>
2022-09-10 19:35:17 +0300

    [#31] - chat bot basic impl

--- app/controllers/api/chatbot/v1/chatbot_controller.rb
+++ app/controllers/api/chatbot/v1/chatbot_controller.rb
@@ -1,13 +1,14 @@
 class Api::Chatbot::V1::ChatbotController < ApplicationController
 
-
-  @@connection_xargs = TomSetting.find_by(agentname: 'github')
-  @@basic_url = connection_xargs.issues_info_url.sub! '#repo_fullname'
+  @tokens = nil
+  @call_count = 0
+  @connection_xargs = TomSetting.find_by(agentname: 'github')
+  @basic_url = @connection_xargs.issues_info_url.sub! '#repo_fullname'
   def welcome_issue
     issue_body = 'some-body'
 
     response = HTTP[accept: 'application/vnd.github.v3+json', Authorization: "token #{getNextToken}"].post(
-      @@basic_url, json: { title: "Welcome issue over repo", body: issue_body })
+      @basic_url, json: { title: "Welcome issue over repo", body: issue_body })
 
     if response.code == 201
       render json: { message: 'Call caught' }, status: 200
@@ -15,6 +16,17 @@ class Api::Chatbot::V1::ChatbotController < ApplicationController
       puts 'Error creating an ticket'
       puts JSON.pretty_generate(response.parse)
       render json: { message: 'Something went wrong' }, status: 404
+    end
+  end
+
+  def initialize
+    puts 'initialize Chatbot Daemon'
+    get_tokens
+  end
+
+  def get_tokens
+    if @tokens.nil?
+      @tokens = TomTokensQueue.where(source: 'github').where(isactive: 'Y')
     end
   end
 
