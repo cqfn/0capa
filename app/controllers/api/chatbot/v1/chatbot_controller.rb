36f9827ea6c32244e341c4640512323fe1e3e109
doldrums <doldrums.317@gmail.com>
2022-09-10 19:30:38 +0300

    =chatbot

--- app/controllers/api/chatbot/v1/chatbot_controller.rb
+++ app/controllers/api/chatbot/v1/chatbot_controller.rb
@@ -1,13 +1,13 @@
 class Api::Chatbot::V1::ChatbotController < ApplicationController
 
-  def welcome_issue
-    connection_xargs = TomSetting.find_by(agentname: 'github')
-    basic_url = connection_xargs.issues_info_url.sub! '#repo_fullname'
 
+  @@connection_xargs = TomSetting.find_by(agentname: 'github')
+  @@basic_url = connection_xargs.issues_info_url.sub! '#repo_fullname'
+  def welcome_issue
     issue_body = 'some-body'
 
     response = HTTP[accept: 'application/vnd.github.v3+json', Authorization: "token #{getNextToken}"].post(
-      basic_url, json: { title: "Welcome issue over repo", body: issue_body })
+      @@basic_url, json: { title: "Welcome issue over repo", body: issue_body })
 
     if response.code == 201
       render json: { message: 'Call caught' }, status: 200
