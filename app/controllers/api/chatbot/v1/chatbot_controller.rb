a22c40b91135603949e26d947c6dd1a53c3def1e
doldrums <doldrums.317@gmail.com>
2022-09-10 18:55:30 +0300

    [#31] - chat bot basic impl

--- app/controllers/api/chatbot/v1/chatbot_controller.rb
+++ app/controllers/api/chatbot/v1/chatbot_controller.rb
@@ -1,4 +1,24 @@
 class Api::Chatbot::V1::ChatbotController < ApplicationController
-  def index
+  def start_chatbot
+    connection_xargs = TomSetting.find_by(agentname: 'github')
+    basic_url = connection_xargs.issues_info_url.sub! '#repo_fullname'
+
+    issue_body = 'some-body'
+
+    response = HTTP[accept: 'application/vnd.github.v3+json', Authorization: "token #{getNextToken}"].post(
+      basic_url, json: { title: "Welcome issue over repo", body: issue_body })
+
+    if response.code == 201
+      render json: { message: 'Call caught' }, status: 200
+    else
+      puts 'Error creating an ticket'
+      puts JSON.pretty_generate(response.parse)
+      render json: { message: 'Something went wrong' }, status: 404
+    end
+  end
+
+  def stop_chatbot
+
+    render json: { message: 'Call caught' }, status: 200
   end
 end
