2b2f1951a6873f26e894a7f2f92df631b5117543
doldrums <doldrums.317@gmail.com>
2022-11-25 01:19:47 +0300

    fix chatbot controller

--- app/controllers/api/chatbot/v1/chatbot_controller.rb
+++ app/controllers/api/chatbot/v1/chatbot_controller.rb
@@ -1,44 +1,167 @@
-class Api::Chatbot::V1::ChatbotController < ApplicationController
+module Api
+  module Chatbot
+    module V1
+      class ChatbotController < ApplicationController
+        @@Tokens = nil
+        @@call_count = 0
+        @base_uri = 'https://api.github.com/repos/xavzelada/repo_test/issues'
 
-  @tokens = nil
-  @call_count = 0
-  @connection_xargs = TomSetting.find_by(agentname: 'github')
-  @basic_url = @connection_xargs.issues_info_url.sub! '#repo_fullname'
-  def welcome_issue
-    issue_body = 'some-body'
+        def initialize
+          puts 'initialize GithubRadar'
+          get_tokens
+        end
 
-    response = HTTP[accept: 'application/vnd.github.v3+json', Authorization: "token #{getNextToken}"].post(
-      @basic_url, json: { title: "Welcome issue over repo", body: issue_body })
+        def get_tokens
+          puts 'Getting token list!!'
+          @@Tokens = TomTokensQueue.where(source: 'github')
+        end
 
-    if response.code == 201
-      render json: { message: 'Call caught' }, status: 200
-    else
-      puts 'Error creating an ticket'
-      puts JSON.pretty_generate(response.parse)
-      render json: { message: 'Something went wrong' }, status: 404
+        def start_chatbot
+          #create
+          welcome_issue
+
+        end
+
+        def welcome_issue
+          issue_body = 'some-body'
+
+          # response = HTTP[accept: 'application/vnd.github.v3+json', Authorization: "token #{getNextToken}"].post(
+          #   @basic_url, json: { title: "Welcome issue over repo", body: issue_body }
+          # )
+
+          response = HTTP[accept: 'application/vnd.github.v3+json', Authorization: "token #{getNextToken}"].post(
+            'https://api.github.com/repos/xavzelada/repo_test/issues', json: { title: "Welcome issue over repo",
+                                                                               body: "some body" }
+          )
+
+          if response.code == 201
+            render json: { message: 'Call caught' }, status: 200
+          else
+            puts 'Error creating an ticket'
+            puts JSON.pretty_generate(response.parse)
+            render json: { message: 'Something went wrong' }, status: 404
+          end
+        end
+
+        def create
+          settings = TomSetting.find_by(agentname: 'github')
+
+          TomRadarActivation.where(status: 'Pending').each do |activation|
+            puts activation.id
+            response = HTTP[accept: 'application/vnd.github.v3+json', Authorization: "token #{getNextToken}"].post(
+              'https://api.github.com/repos/xavzelada/repo_test/issues', json: { title: activation.issuetitle,
+                                                                                 body: activation.issuebody }
+            )
+            puts JSON.pretty_generate(response)
+            if response.code == 201
+              activation.status = 'Success'
+              activation.save
+
+              json = JSON.parse(response)
+
+              myIssue = TomIssue.new(
+                issueid: json['id'],
+                repo_issueid: json['number'],
+                created_at_ext: json['created_at']
+              )
+
+              myIssue.save
+            else
+              print('Error creating an ticket')
+              puts JSON.pretty_generate(response.parse)
+            end
+            # puts response
+          end
+
+          render json: { message: 'Posting issues finished' }, status: 200
+        end
+
+        def update_issue
+          request.body.rewind
+          json = JSON.parse(
+            case request.content_type
+            when 'application/x-www-form-urlencoded'
+              params[:payload]
+            when 'application/json'
+              request.body.read
+            else
+              raise "Invalid content-type: \"#{request.content_type}\""
+            end
+          )
+          if json['action'] == 'closed'
+            myIssue = TomIssue.find_by(issueid: json['issue']['id'])
+            myIssue.comments = json['issue']['comments']
+            # myIssue.closed_by = json["issue"]["comments"]
+            myIssue.closed_at_ext = json['issue']['closed_at']
+            if myIssue.save
+              render json: { message: 'Call caught' }, status: 201
+            else
+              render json: { error: 'Error processing the data' }, status: 400
+            end
+          else
+            render json: { message: 'Action no tracker' }, status: 201
+          end
+        rescue Exception => e
+          render json: { error: e.message }, status: 422
+        end
+
+        def create_github_issues
+          settings = TomSetting.find_by(agentname: 'github')
+
+          TomProjectCapaPrediction.where(status: 'N').each do |prediction|
+            # posted
+            # prediction.status = "P"
+            puts prediction.id
+
+            info_url_template = settings.issues_info_url.dup
+
+            request_url = info_url_template.sub! '#repo_fullname', prediction.repo_fullname
+
+            issue_body = 'TOM has finished to check you code and it would like to advise you with some actions:'
+
+            capas = TomCapaDictonary.where(case_id: prediction.label).each do |capa|
+              issue_body = "#{issue_body}
+  - #{capa.description}"
+            end
+
+            puts issue_body
+            prediction.save
+            response = HTTP[accept: 'application/vnd.github.v3+json', Authorization: "token #{getNextToken}"].post(
+              request_url, json: { title: "TOM Findings over repo ##{prediction.repo_fullname}",
+                                   body: issue_body }
+            )
+
+            puts JSON.pretty_generate(response)
+
+            if response.code == 201
+              prediction.status = 'P'
+              prediction.save
+
+              json = JSON.parse(response)
+
+              myIssue = TomIssue.new(
+                issueid: json['id'],
+                repo_issueid: json['number'],
+                created_at_ext: json['created_at']
+              )
+
+              myIssue.save
+            else
+              print('Error creating an ticket')
+              puts JSON.pretty_generate(response.parse)
+            end
+            puts response
+          end
+
+          render json: { message: 'Posting issues finished' }, status: 200
+        end
+
+        def getNextToken
+          @@call_count += 1
+          next_token_index = (@@call_count % @@Tokens.length)
+          @@Tokens[next_token_index].token
+        end
+      end
     end
-  end
-
-  def initialize
-    puts 'initialize Chatbot Daemon'
-    get_tokens
-  end
-
-  def get_tokens
-    if @tokens.nil?
-      @tokens = TomTokensQueue.where(source: 'github').where(isactive: 'Y')
-    end
-  end
-
-  def start_chatbot
-    welcome_issue
-    while true:
-      # тут спамим ишьюсами получается
-    end
-  end
-
-  def stop_chatbot
-
-    render json: { message: 'Call caught' }, status: 200
   end
 end
